b1a1fbd31a8bed9e681e8ae24388598acb8cc837
_Aking <1179414687@qq.com>
2026-04-22 00:02:53 +0800

    Update base URL configuration in vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  base: '/drawnix/',
+  base: process.env.BASE_URL || '/',
   cacheDir: '../../node_modules/.vite/apps/web',
 
   server: {
